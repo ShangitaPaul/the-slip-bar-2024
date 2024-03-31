c25a022771aec3882eb40e7ce6eaaa159bcf73a7
Shangita Paul <shangita.paul@gmail.com>
2024-03-31 11:42:57 -0700

    Update JobsPage.js
    added correct jobspage css

--- the-slip-bar-react-webpage/src/components/JobsPage.js
+++ the-slip-bar-react-webpage/src/components/JobsPage.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import JobList from './JobList';
 import JobDetails from './JobDetail'; // Assuming JobDetails component exists
-import '../styles/JobsPage.css';
+import '../styles/jobsPage.css';
 
 function JobsPage() {
   const [jobs, setJobs] = useState([]);

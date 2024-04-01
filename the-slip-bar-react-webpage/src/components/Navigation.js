dfed4cff895e506b79c6324fd2b646e084588194
Shangita Paul <shangita.paul@gmail.com>
2024-04-01 11:48:12 -0700

    edited ig icon to be transparant

--- the-slip-bar-react-webpage/src/components/Navigation.js
+++ the-slip-bar-react-webpage/src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
 import "../styles/Navigation.css";
 
 function Navigation() {
